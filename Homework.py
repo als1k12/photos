7c94eb467184241d5689f5559d416fa1fec2b298
als1k12 <alskovalsk@gmail.com>
2026-03-01 16:12:12 +0300

    Create Homework.py

---
+++ Homework.py
@@ -0,0 +1,28 @@
+import time, threading, schedule, telebot, random
+from telebot import TeleBot
+import images_memes
+import images_nomemes
+
+token = "8263397450:AAEG9A_UUCijklkBJLFnvJQ6lA2kdgbtgO4"
+bot = telebot.TeleBot("8263397450:AAEG9A_UUCijklkBJLFnvJQ6lA2kdgbtgO4")
+
+
+mem_images = ['images_memes/kot.jpg' , 'images_memes/ded.jpg' , 'images_memes/monkey.jpg']
+nomemes_images = ['images_nomemes/kot.jpg' , 'images_nomemes/kot2.jpg' , 'images_nomemes/kot3.jpg']
+
+
+@bot.message_handler(commands=['mem'])
+def send_mem(message):
+     img = random.choice(mem_images)
+     with open(img, 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+
+
+@bot.message_handler(commands=['photo'])
+def send_photo(message):
+     img2 = random.choice(nomemes_images)
+     with open(img2, 'rb') as f:
+        bot.send_photo(message.chat.id, f)
+
+print("Бот запущен.")
+bot.polling()
